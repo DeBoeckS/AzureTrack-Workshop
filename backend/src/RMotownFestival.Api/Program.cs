27cab3e4a7c70f040142c51989220c13c68b35b7
Simon De Boeck <simondeboeck1@hotmail.com>
2020-12-09 11:54:11 +0100

    Remove connectionstring from appsettings. Use the key vault instead.

--- backend/src/RMotownFestival.Api/Program.cs
+++ backend/src/RMotownFestival.Api/Program.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace RMotownFestival.Api
 {
@@ -19,9 +16,21 @@ namespace RMotownFestival.Api
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+            .ConfigureAppConfiguration((context, config) =>
+            {
+                if (!context.HostingEnvironment.IsDevelopment())
                 {
-                    webBuilder.UseStartup<Startup>();
-                });
+                    IConfiguration configuration = config.Build();
+
+                    var tokenProvider = new AzureServiceTokenProvider();
+                    var client = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(tokenProvider.KeyVaultTokenCallback));
+
+                    config.AddAzureKeyVault($"https://{configuration["AzureKeyVaultName"]}.vault.azure.net/", client, new DefaultKeyVaultSecretManager());
+                }
+            })
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
